efafedd515d21210194d7cd1d4cbfabfe980b68e
Niko Lowry <niko@neeks.me>
2026-05-16 21:04:41 -0400

    fix(mpd): Check for mpc command to avoid false positive with proxy fn

--- home/shell/env/mpd.sh
+++ home/shell/env/mpd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-[[ $(pidof mpd) ]] &&
-    [[ $(command -v mpc) ]] &&
+[[ $(command -v mpc) ]] &&
+    [[ $(pidof mpd) ]] &&
     [[ ! $(mpc current) ]] &&
     mpc searchplay filename "$(mpc listall | shuf -n 1)" &>/dev/null &&
     mpc pause &>/dev/null
